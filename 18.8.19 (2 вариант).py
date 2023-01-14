dfc7c5fd7b55d85d086a5e1e5862dc68d18e7e4d
KatarinaGrinya <119685916+KatarinaGrinya@users.noreply.github.com>
2023-01-14 10:55:20 +0300

    Create 18.8.19 (2 вариант).py

---
+++ 18.8.19 (2 вариант).py
@@ -0,0 +1,31 @@
+tickets = int(input('Введите количество билетов: '))
+
+
+ages = []
+
+
+for i in range(0, tickets):
+    input_value = int(input(f'Введите возраст {i + 1} участника: '))
+    ages.append(input_value)
+
+
+    def ticket_prise(age):
+        if age < 18:
+            return 0
+        elif 18 <= age < 25:
+            return 990
+        elif 25 <= age:
+            return 1390
+
+
+    total_ = sum(map(ticket_prise, ages))
+
+
+discount = int(total_ * 0.1)
+discounted_prise = int(total_ - discount)
+if tickets >= 3:
+    print('Полная стоимость билетов: ', total_, "руб.")
+    print('Сумма скидки (при регистрации более 3 участников предоставляется скидка 10%): ', discount, "руб.")
+    print('Общая стоимость билетов со скидкой: ', discounted_prise, "руб.")
+else:
+    print('Общая стоимость билетов: ', total_, "руб.")
